4e715ee1655d7042c4cdc8e92a44cc1b2cf8e2f6
vmarinkov <dp.lmnt@gmail.com>
2014-02-19 21:17:33 +0200

    Update IniParser.java

--- src/IniParser.java
+++ src/IniParser.java
@@ -7,76 +7,82 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Class for parsing the ini files
- * 
+ * Class for parsing INI files.
+ *
  * @author Miglen Evlogiev
  */
 public class IniParser {
 
-   private Pattern  _section  = Pattern.compile( "\\s*\\[([^]]*)\\]\\s*" );
-   private Pattern  _keyValue = Pattern.compile( "\\s*([^=]*)=(.*)" );
-   private Map< String,
-      Map< String,
-         String >>  _entries  = new HashMap<>();
+    private Pattern _section = Pattern.compile("\\s*\\[([^]]*)\\]\\s*");
+    private Pattern _keyValue = Pattern.compile("\\s*([^=]*)=(.*)");
+    private Map<String, Map<String, String>> _entries = new HashMap<String, Map<String, String>>();
 
-   public IniParser( String path ) throws IOException {
-      load( path );
-   }
+    public IniParser(String path) throws IOException {
+        load(path);
+    }
 
-   public void load( String path ) throws IOException {
-      try( BufferedReader br = new BufferedReader( new FileReader( path ))) {
-         String line;
-         String section = null;
-         while(( line = br.readLine()) != null ) {
-            Matcher m = _section.matcher( line );
-            if( m.matches()) {
-               section = m.group( 1 ).trim();
+    public void load(String path) throws IOException {
+        try {
+            String line;
+            String section = null;
+
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            try {
+                while ((line = br.readLine()) != null) {
+                    Matcher m = _section.matcher(line);
+                    if (m.matches()) {
+                        section = m.group(1).trim();
+                    } else if (section != null) {
+                        m = _keyValue.matcher(line);
+                        if (m.matches()) {
+                            String key = m.group(1).trim();
+                            String value = m.group(2).trim();
+                            Map< String, String> kv = _entries.get(section);
+                            if (kv == null) {
+                                _entries.put(section, kv = new HashMap<String, String>());
+                            }
+                            kv.put(key, value);
+                        }
+                    }
+                }
+            } finally {
+                br.close();
             }
-            else if( section != null ) {
-               m = _keyValue.matcher( line );
-               if( m.matches()) {
-                  String key   = m.group( 1 ).trim();
-                  String value = m.group( 2 ).trim();
-                  Map< String, String > kv = _entries.get( section );
-                  if( kv == null ) {
-                     _entries.put( section, kv = new HashMap<>());   
-                  }
-                  kv.put( key, value );
-               }
-            }
-         }
-      }
-   }
+        } catch (IOException ex) {
 
-   public String getString( String section, String key, String defaultvalue ) {
-      Map< String, String > kv = _entries.get( section );
-      if( kv == null ) {
-         return defaultvalue;
-      }
-      return kv.get( key );
-   }
+        }
+    }
 
-   public int getInt( String section, String key, int defaultvalue ) {
-      Map< String, String > kv = _entries.get( section );
-      if( kv == null ) {
-         return defaultvalue;
-      }
-      return Integer.parseInt( kv.get( key ));
-   }
+    public String getString(String section, String key, String defaultvalue) {
+        Map< String, String> kv = _entries.get(section);
+        if (kv == null) {
+            return defaultvalue;
+        }
+        return kv.get(key);
+    }
 
-   public float getFloat( String section, String key, float defaultvalue ) {
-      Map< String, String > kv = _entries.get( section );
-      if( kv == null ) {
-         return defaultvalue;
-      }
-      return Float.parseFloat( kv.get( key ));
-   }
+    public int getInt(String section, String key, int defaultvalue) {
+        Map< String, String> kv = _entries.get(section);
+        if (kv == null) {
+            return defaultvalue;
+        }
+        return Integer.parseInt(kv.get(key));
+    }
 
-   public double getDouble( String section, String key, double defaultvalue ) {
-      Map< String, String > kv = _entries.get( section );
-      if( kv == null ) {
-         return defaultvalue;
-      }
-      return Double.parseDouble( kv.get( key ));
-   }
+    public float getFloat(String section, String key, float defaultvalue) {
+        Map< String, String> kv = _entries.get(section);
+        if (kv == null) {
+            return defaultvalue;
+        }
+        return Float.parseFloat(kv.get(key));
+    }
+
+    public double getDouble(String section, String key, double defaultvalue) {
+        Map< String, String> kv = _entries.get(section);
+        if (kv == null) {
+            return defaultvalue;
+        }
+        return Double.parseDouble(kv.get(key));
+    }
 }
